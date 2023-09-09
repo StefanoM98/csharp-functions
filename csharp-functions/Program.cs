ab38684a183fbd663d190e580ed0a5644833ac72
StefanoM98 <Utente@PC-STEFANO>
2023-09-09 18:54:17 +0200

    Aggiungere i file di progetto.

---
+++ csharp-functions/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace csharp_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeriArray = {1, 2, 3, 4, 5, 6, 7};
+            PrintArray(numeriArray);
+            //Funzioni
+            void PrintArray(int[] array)
+            {
+                Console.Write("[");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(array[i] + ", ");
+                    }
+                    else
+                    {
+                        Console.Write(array[i]);
+                    }
+                }
+
+                Console.WriteLine("]");
+
+            }
+        }
+    }
+}
